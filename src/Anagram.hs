655f2967c9c5ceb899516e827d1edcc7b17f0b28
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-08 14:45:29 +0200

    solution

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -1,4 +1,10 @@
 module Anagram (anagramsFor) where
 
+import Data.Char (toLower)
+import Data.List (sort)
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor xs = filter (\c -> (sort (map toLower c) == sort (map toLower xs)) && not (equalsIgnoreCase xs c))
+
+equalsIgnoreCase :: String -> String -> Bool
+equalsIgnoreCase s1 s2 = map toLower s1 == map toLower s2
