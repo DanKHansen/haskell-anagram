8ef17e498a48fac0aee66aaa20b7992aa86b2401
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-08 14:56:39 +0200

    refactor

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -4,7 +4,4 @@ import Data.Char (toLower)
 import Data.List (sort)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs = filter (\c -> (sort (map toLower c) == sort (map toLower xs)) && not (equalsIgnoreCase xs c))
-
-equalsIgnoreCase :: String -> String -> Bool
-equalsIgnoreCase s1 s2 = map toLower s1 == map toLower s2
+anagramsFor xs = filter (\x -> (sort (map toLower x) == sort (map toLower xs)) && map toLower xs /= map toLower x)
